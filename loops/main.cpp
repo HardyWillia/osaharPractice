0e7e9f103f69db81cd4c9543505f7f18b714f44a
HardyWillia <hardywillia@outlook.com>
2018-09-19 21:18:11 -0400

    Added code to calculate letter grade

--- loops/main.cpp
+++ loops/main.cpp
@@ -7,6 +7,7 @@
 
 #include <iostream>
 #include <iomanip>
+#include <string>
 
 using namespace std;
 
@@ -22,7 +23,7 @@ int main(){
     double totalptspossible;
     double overall;
 
-    char lettergrade;
+    string lettergrade;
 
     //Prompt the user
     cout << "How many exercises to input?: ";
@@ -42,18 +43,40 @@ int main(){
 
     }
 
+    //Calculate the Overall Percentage
     overall = (totalreceived / totalptspossible) * 100;
 
-
+    //Output total score
     cout << "Your total is " << totalreceived << " out of " << totalptspossible << ", or " << overall << "%" << endl;
 
+    //Calculate the Letter grade
 
+    if(overall >= 93)
+        lettergrade = "A";
+                if(overall < 93 || overall >= 90)
+                    lettergrade = "A-";
+                if(overall < 90 || overall >= 87)
+                    lettergrade = "B+";
+                if(overall < 87 || overall >= 83)
+                    lettergrade = "B";
+                if(overall < 83 || overall >= 80)
+                    lettergrade = "B-";
+                if(overall < 80 || overall >= 77)
+                    lettergrade = "C+";
+                if(overall < 77 || overall >= 73)
+                    lettergrade = "C";
+                if(overall < 73 || overall >= 70)
+                    lettergrade = "C-";
+                if(overall < 70 || overall >= 67)
+                    lettergrade = "D+";
+                if(overall < 67 || overall >= 63)
+                    lettergrade = "D";
+                if(overall < 63 || overall >= 60)
+                    lettergrade = "D-";
+        else 
+                    lettergrade = "F";
 
-
-
-
-
-
+        cout << "Your letter grade is: " << lettergrade << endl;
 
 
     return 0;
